6e90ed1793fe961738626956c27e84cf13977f81
Christopher Salinas <csalinas@iteamnm.com>
2017-07-05 10:55:06 -0600

    refactor: use object instead of key value pairs

--- iTEAMConsulting.FormHandler.Abstractions/IMakeEmails.cs
+++ iTEAMConsulting.FormHandler.Abstractions/IMakeEmails.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections.Generic;
-
+﻿
 namespace iTEAMConsulting.FormHandler.Abstractions
 {
     public interface IMakeEmails
     {
-        string Build(IEnumerable<KeyValuePair<string, string>> formData);
+        string Build(object formData);
     }
 }
--- iTEAMConsulting.FormHandler/MakeEmails.cs
+++ iTEAMConsulting.FormHandler/MakeEmails.cs
@@ -1,12 +1,20 @@
 ﻿using iTEAMConsulting.FormHandler.Abstractions;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace iTEAMConsulting.FormHandler
 {
     public class MakeEmails : IMakeEmails
     {
-        public string Build(IEnumerable<KeyValuePair<string, string>> formData)
+        /// <summary>
+        /// Build accepts a plain object and produces a string of HTML suitable for use as an email message body.
+        /// Build will reflect the object's public properties, use the property name as a key, and the property
+        /// value as the value.  This set of properties will be used for the data in the HTML string.
+        /// </summary>
+        /// <param name="formData">An object with at least one public property.</param>
+        /// <returns>An email friendly HTML string</returns>
+        public string Build(object formData)
         {
             throw new NotImplementedException();
         }
